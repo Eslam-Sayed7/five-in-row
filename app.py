16ee154d8f8ee9b16194800642734d697a4c7387
Yousef-Albasel <albaselofficial@gmail.com>
2025-05-10 04:28:59 +0300

    Closed dramatic mode

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from controllers.GameEngine import GameEngine
 
 game = GameEngine()
-game.dramatic_mode = True # Don't turn it to true unless you are not debugging
+game.dramatic_mode = False # Don't turn it to true unless you are not debugging
 game.run()
 
